99236764c0f2a6a7b3c8f917083d7a10801e0a05
IveBeatenTetris <ive_beaten_tetris@geekmail.de>
2018-08-16 19:42:29 +0200

    made player facing a position

--- player.py
+++ player.py
@@ -32,15 +32,17 @@ class Player(pg.sprite.Sprite):
         self.image = self.frames[0]# pygame.surface
         self.rect = self.image.get_rect()# pygame.rect
         self.speed = 2# int
+        self.facing = "down"# str
         # drawing a border around the player
-        draw(
-            drawBorder(self.rect, [1, "solid", (255, 0, 0)]),
-            self.image,
-            (0, 0)
-            )
+        # draw(
+        #     drawBorder(self.rect, [1, "solid", (255, 0, 0)]),
+        #     self.image,
+        #     (0, 0)
+        #     )
     def __repr__(self):# str
         """String representation."""
         return "<Player({0})".format(str(self.rect.topleft))
+    # //TODO get this collision shit together
     def __moveSingleAxis(self, pos, blocks):
         """Dirty method to check for collisions and moving the player to the
         right position."""
@@ -63,9 +65,25 @@ class Player(pg.sprite.Sprite):
 
         if keys[pg.K_a]:
             self.__moveSingleAxis((-self.speed, 0), blocks)
+            self.facing = "left"
         if keys[pg.K_d]:
             self.__moveSingleAxis((self.speed, 0), blocks)
+            self.facing = "right"
         if keys[pg.K_w]:
             self.__moveSingleAxis((0, -self.speed), blocks)
+            self.facing = "up"
         if keys[pg.K_s]:
             self.__moveSingleAxis((0, self.speed), blocks)
+            self.facing = "down"
+
+        self.turn()
+    def turn(self):
+        """."""
+        if self.facing == "down":
+            self.image = self.frames[0]
+        elif self.facing == "left":
+            self.image = self.frames[1]
+        elif self.facing == "up":
+            self.image = self.frames[2]
+        elif self.facing == "right":
+            self.image = self.frames[3]
